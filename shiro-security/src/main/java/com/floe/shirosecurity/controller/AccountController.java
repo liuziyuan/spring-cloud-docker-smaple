ba1d5544c27958f14c9886c9a05c3a7f829d4b60
liuziyuan <liuziyuan.room@gmail.com>
2019-05-29 14:22:38 +0800

    modify

--- shiro-security/src/main/java/com/floe/shirosecurity/controller/AccountController.java
+++ shiro-security/src/main/java/com/floe/shirosecurity/controller/AccountController.java
@@ -11,15 +11,16 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
 
 	private Map<String, Object> map;
